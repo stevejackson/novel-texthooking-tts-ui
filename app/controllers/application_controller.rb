d1d7d263b89a9aa6c9ee9d6ca1373d5765a15425
steven <steve@sjackson.net>
2025-05-25 02:17:56 -0400

    Add basic http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: Rails.application.credentials.http_basic_auth.user!,
+                               password: Rails.application.credentials.http_basic_auth.password!
 end
