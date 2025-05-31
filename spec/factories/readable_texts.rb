cd8d52ff07dbc9803f7d0c310bb79d429f75e038
steven <steve@sjackson.net>
2025-05-31 14:54:34 -0400

    Remove unused spec boilerplate

--- spec/factories/readable_texts.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :readable_text do
-    name { "MyString" }
-    language { "MyString" }
-    content { "MyText" }
-  end
-end
--- spec/factories/texts.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :text do
-    name { "MyString" }
-    language { "MyString" }
-    content { "MyText" }
-  end
-end
